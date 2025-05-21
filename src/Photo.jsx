82ed5bdc1a8f01b413a9643e26acf92c3e4920c8
vishaltu45 <82863840+vishaltu45@users.noreply.github.com>
2025-05-21 20:36:37 +0100

    Update Photo.jsx

--- src/Photo.jsx
+++ src/Photo.jsx
@@ -7,25 +7,25 @@ import { motion } from 'framer-motion';
 import './Photo.css'; // For any custom styles
 
 const photos = [
-  'photos/photo1.png',
-  'photos/photo2.png',
-  'photos/photo3.png',
-  'photos/photo4.png',
-  'photos/photo5.png',
-  'photos/photo6.png',
-  'photos/photo7.png',
-  'photos/photo8.png',
-  'photos/photo9.png',
-  'photos/photo10.png',
-  'photos/photo11.png',
-  'photos/photo12.png',
-  'photos/photo13.png',
+  './photos/photo1.png',
+  './photos/photo2.png',
+  './photos/photo3.png',
+  './photos/photo4.png',
+  './photos/photo5.png',
+  './photos/photo6.png',
+  './photos/photo7.png',
+  './photos/photo8.png',
+  './photos/photo9.png',
+  './photos/photo10.png',
+  './photos/photo11.png',
+  './photos/photo12.png',
+  './photos/photo13.png',
   
   
   // Add up to 20
 ];
 
-const flipSound = new Audio('sounds/page.mp3');
+const flipSound = new Audio('./sounds/page.mp3');
 
 function FlipbookAlbum() {
   const book = useRef();
